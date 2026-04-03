e831c1d8a0d0c16da9a1e3c06b5355c5fa6a7974
= <=>
2026-04-03 15:03:00 -0500

    added bank details

--- src/app/bank.ts
+++ src/app/bank.ts
@@ -1,2 +1,5 @@
 export interface Bank {
+    id: number; 
+    name: string;
+    website: string;
 }
---
+++ src/app/banks.ts
@@ -0,0 +1,13 @@
+import { Bank } from './bank';
+
+export const BANKS: Bank[] = [ 
+   { id: 11, name: 'Bank of America Merrill Lynch', website: 'bankofamerica.com'}, 
+   { id: 12, name: 'Barclays Capital', website: 'barclays.com' }, 
+   { id: 13, name: 'Citi', website: 'citi.com' }, 
+   { id: 14, name: 'Credit Suisse', website: 'creditsuisse.com'}, 
+   { id: 15, name: 'Deutsche Bank', website: 'deutschebank.com' }, 
+   { id: 16, name: 'Goldman Sachs', website: 'goldmansachs.com' }, 
+   { id: 17, name: 'J.P. Morgan', website: 'jpmorgan.com' }, 
+   { id: 18, name: 'Morgan Stanley', website: 'morganstanley.com' }, 
+   { id: 19, name: 'UBS', website: 'ubs.com' }
+];
